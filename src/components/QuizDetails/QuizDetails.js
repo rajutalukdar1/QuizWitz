60b848198efdcc203d8d81ac4c85897e959112da
rajutalukdar1 <lalutaluker@gmail.com>
2022-10-11 23:47:37 +0600

    responsiv work done

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -19,7 +19,7 @@ const QuizDetails = ({ quiz }) => {
             <div className='border-solid bg-blue-100 rounded-md border-black my-4 border w-1/2 mx-auto'>
                 <h3 className='text-xl mb-4 font-semibold'>{question}</h3>
 
-                <div className='questions'>
+                <div className='questions grid sm:grid-cols-1 lg:grid-cols-2'>
                     {
                         options.map(qus => <Qus
                             key={qus.toString()} value={qus}
