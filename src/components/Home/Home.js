9eab290c7b82cf18f55463f3cf6d7baa91246c05
rajutalukdar1 <lalutaluker@gmail.com>
2022-10-12 23:15:08 +0600

    all done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
             <div className='home-photos lg:w-4/5 sm:w-3/4'>
                 <p className='text-3xl pt-12 font-bold text-cyan-400'>Check your development knowledge</p>
                 <p className='text-xl pt-8 font-semibold text-teal-500'>Answer the questions on our website to test your knowledge, <br /> and let your knowledge be tested</p>
-                <p className='text-xl font-semibold text-cyan-400'>You can take tests on multiple subjects here, and you can easily check the <br /> knowledge you have learned</p>
+                <p className='text-xl font-semibold text-cyan-400'>You can take tests on multiple subjects here,  and you can easily check the <br /> knowledge you have learned</p>
             </div>
             <div className='mx-auto my-12 gap-8 grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 w-4/5'>
                 {
